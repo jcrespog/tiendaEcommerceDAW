8e16dee41769ea541b4575015ac0685b1f2fc905
carlos <carlos198069@gmail.com>
2019-01-27 23:13:08 +0100

    agregado el footer con los datos de contacto y mapa con la ubicación

--- inicio/contacto.php
+++ inicio/contacto.php
@@ -1,5 +1,70 @@
 <?php include '../extend/header.php';?>
 
+<!-- footer -->
+<div class="container mt-5 mb-4 text-center text-md-left">
+    <div class="row mt-6">
+
+        <!-- Mapa -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-6">
+            <h6 class="text-uppercase font-weight-bold">
+                <strong>Mapa</strong>
+            </h6>
+            <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3206.6631214547288!2d-6.28319194418253!3d36.51399328002556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0dd3cb5b9598fd%3A0x1b70c9599f25f5a2!2sConsulta+Privada+De+Bienvenido+Gir%C3%B3n+Nuria!5e0!3m2!1ses!2ses!4v1548626464253" width="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            
+        </div>
+        <!-- /mapa -->
+
+        <!-- Contacto-->
+        <div class="col-md-4 col-lg-3 col-xl-3">
+            <h6 class="text-uppercase font-weight-bold">
+                <strong>Contacto</strong>
+            </h6>
+            <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+            <p>
+                <i class="fas fa-home  mr-3"></i> España, Cádiz 11540</p>
+            <p>
+                <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+            <p>
+                <i class="fas fa-phone mr-3"></i> + 34 689 498 883</p>
+            <p>
+                <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- /contacto -->
+    </div>
+</div>
+<!--/ footer -->
+
+    <!-- redes sociales -->
+    <div class="primary-color">
+        <div class="container">
+            <div class="row py-4 d-flex align-items-center">
+                <div class="col-md-6 col-lg-7 text-center text-md-right">
+                    <!--Facebook-->
+                    <a class="fb-ic ml-0">
+                        <i class="fab fa-facebook-f white-text mr-4"> </i>
+                    </a>
+                    <!--Twitter-->
+                    <a class="tw-ic">
+                        <i class="fab fa-twitter white-text mr-4"> </i>
+                    </a>
+                    <!--Google +-->
+                    <a class="gplus-ic">
+                        <i class="fab fa-google-plus-g white-text mr-4"> </i>
+                    </a>
+                    <!--Linkedin-->
+                    <a class="li-ic">
+                        <i class="fab fa-linkedin-in white-text mr-4"> </i>
+                    </a>
+                    <!--Instagram-->
+                    <a class="ins-ic">
+                        <i class="fab fa-instagram white-text mr-lg-4"> </i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /redes sociales -->
 
 <?php include '../extend/footer.php';?>
 </body>
